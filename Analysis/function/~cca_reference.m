816041f3ac4ded27571803e06ec2af5009d0b2db
ALONELUR <yinwenhan1998@gmail.com>
2019-05-01 15:27:41 +0800

    把叠加ref的相位改成了固定的四分之pi

--- Analysis/function/~cca_reference.m
+++ Analysis/function/~cca_reference.m
@@ -19,7 +19,7 @@ Explain : TargetNum:目标刺激频率数量
     else
         HarmList = harms(1):1:harms(2);
     end
-    harms_num = size(harms,2);
+    harms_num = size(HarmList,2);
 
     t = linspace(0,sample_num/fs-1/fs,sample_num);
 
@@ -38,7 +38,7 @@ Explain :
     for targ_i = 1:TargetNum
         temp = zeros(channelNum, sample_num);
         for harm_i = 1:harms_num
-            temp = temp + exp(-0.7*abs(HarmList(harm_i)))*sin(2*pi*targ_fre(targ_i)*2^HarmList(harm_i)*t+2*pi*rand(channelNum, 1));
+            temp = temp + exp(-0.7*abs(HarmList(harm_i)))*sin(2*pi*targ_fre(targ_i)*2^HarmList(harm_i)*t+2*pi*[0;0.25;0.5;0.75]);
         end
         yef(:,:,targ_i) = temp;
     end
